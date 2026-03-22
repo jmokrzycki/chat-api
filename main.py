817559a6d1640050105e4dd92a2ad6409cad507d
jmokrzycki <jmokrzycki@cppc.gov.pl>
2026-03-22 23:11:00 +0100

    openai standard

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import httpx
+import json
+from typing import cast
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
+from openai import AsyncOpenAI, AsyncStream
+from openai.types.chat import ChatCompletionUserMessageParam, ChatCompletionChunk
 
 app = FastAPI()
 
@@ -13,24 +16,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+client = AsyncOpenAI(
+    base_url="http://localhost:11434/v1",
+    api_key="ollama"
+)
+
 @app.post("/api/chat")
 async def ask_bielik(data: dict):
-    user_prompt = data.get("prompt", "Brak pytania")
+    user_prompt = str(data.get("prompt", "Brak pytania"))
+
+    messages =[ChatCompletionUserMessageParam(role="user", content=user_prompt)]
 
     async def proxy_stream():
-        async with httpx.AsyncClient() as client:
-            async with client.stream(
-                    "POST",
-                    "http://localhost:11434/api/generate",
-                    json={
-                        "model": "speakleash/bielik-11b-v3.0-instruct:Q4_K_M",
-                        "prompt": user_prompt,
-                        "stream": True
-                    },
-                    timeout=None
-            ) as response:
-                async for chunk in response.aiter_bytes():
-                    yield chunk
+        try:
+            response = await client.chat.completions.create(
+                model="speakleash/bielik-11b-v3.0-instruct:Q4_K_M",
+                messages=messages,
+                stream=True
+            )
+
+            stream = cast(AsyncStream[ChatCompletionChunk], cast(object, response))
+
+            async for chunk in stream:
+                if chunk.choices and chunk.choices[0].delta.content is not None:
+                    content = chunk.choices[0].delta.content
+                    yield json.dumps({"response": content}) + "\n"
+
+        except Exception as e:
+            yield json.dumps({"response": f"\n[Błąd połączenia: {str(e)}]"}) + "\n"
 
     return StreamingResponse(
         proxy_stream(),
